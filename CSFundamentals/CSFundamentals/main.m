2bb6567991d15fc0cfc06f70eeb91491e40b075c
Hui,Li <lihui40@baidu.com>
2019-10-02 23:52:23 +0800

    new basic command line project

---
+++ CSFundamentals/CSFundamentals/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CSFundamentals
+//
+//  Created by Hui,Li on 2019/10/2.
+//  Copyright © 2019 Hui,Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
