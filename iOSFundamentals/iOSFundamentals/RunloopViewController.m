1556492457f1fed2de2b8bc9d8d46d7922fc33f6
Hui,Li <nigelathome@126.com>
2020-12-15 12:35:04 +0800

    runloop实现子线程保活

--- iOSFundamentals/iOSFundamentals/RunloopViewController.m
+++ iOSFundamentals/iOSFundamentals/RunloopViewController.m
@@ -10,15 +10,29 @@
 
 @interface RunloopViewController ()
 
+@property(nonatomic, readwrite) NSThread * myThread;
+
 @end
 
 @implementation RunloopViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(print) object:nil];
-    [self performSelector:@selector(print) onThread:thread withObject:nil waitUntilDone:NO];
-    [thread start];
+//    NSThread thread = [[NSThread alloc] initWithTarget:self selector:@selector(print) object:nil];
+//    [self performSelector:@selector(print) onThread:thread withObject:nil waitUntilDone:NO];
+//    [self.thread start];
+    [self runThread];
+}
+
+- (void)runThread {
+    self.myThread = [[NSThread alloc] initWithTarget:self selector:@selector(onThread) object:nil];
+    [self.myThread start];
+}
+
+//给子线程添加runloop
+- (void)onThread {
+    [[NSRunLoop currentRunLoop] addPort:[NSPort new] forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop] run];
 }
 
 - (void)print {
@@ -26,6 +40,15 @@
     NSRunLoop *runloop = [NSRunLoop currentRunLoop];
 //    [runloop run];
     NSLog(@"那里");
+}
+
+- (void)doTask {
+    LGNSLog(@"%s", __FUNCTION__);
+}
+
+//子线程中执行任务 但不需要每次执行完之后就销毁线程 通过runloop实现线程保活
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self performSelector:@selector(doTask) onThread:self.myThread withObject:nil waitUntilDone:NO];
 }
 
 /*
