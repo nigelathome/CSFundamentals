1ec13eb7e6d2ab6ae7db08862c7bb6c08a3c5780
Hui,Li <nigelathome@126.com>
2020-12-25 12:09:01 +0800

    实现AB任务并行 C等待AB执行完才执行：方法一：调度组+group_wait

--- iOSFundamentals/iOSFundamentals/GCDViewController.m
+++ iOSFundamentals/iOSFundamentals/GCDViewController.m
@@ -37,9 +37,10 @@
 //    [self gcdTest18];
 //    [self gcdTest19];
 //    [self gcdTest20];
-    [self gcdTest21];
+//    [self gcdTest21];
 //    [self gcdTest22];
 //    [self gcdTest23];
+    [self ABC];
 }
 
 - (void)gcdTest1 {
@@ -421,5 +422,19 @@
     //1和2任务追加到主队列顺序晚于3此时先执行3，而1是耗时任务会结束的晚于2 所以是2-1
 }
 
+- (void)ABC {
+    //实现这样的需求：A和B两个任务异步执行 C任务需要等待A任务执行完才执行
+    dispatch_group_t g1 = dispatch_group_create();
+    dispatch_group_async(g1, dispatch_get_global_queue(0, 0), ^{
+        LGNSLog(@"A");
+    });
+    dispatch_group_async(g1, dispatch_get_global_queue(0, 0), ^{
+        LGNSLog(@"B");
+    });
+    dispatch_group_wait(g1, DISPATCH_TIME_FOREVER);
+    dispatch_group_async(g1, dispatch_get_global_queue(0, 0), ^{
+        LGNSLog(@"C");
+    });
+}
 
 @end
