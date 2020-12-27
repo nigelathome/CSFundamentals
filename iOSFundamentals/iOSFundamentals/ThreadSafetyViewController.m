44a839038292147cf0765b07e54723097a270495
Hui,Li <nigelathome@126.com>
2020-12-27 16:03:20 +0800

    线程不安全例子

--- iOSFundamentals/iOSFundamentals/ThreadSafetyViewController.m
+++ iOSFundamentals/iOSFundamentals/ThreadSafetyViewController.m
@@ -10,6 +10,8 @@
 
 @interface ThreadSafetyViewController ()
 
+@property (atomic, assign) NSInteger slice;
+
 @end
 
 @implementation ThreadSafetyViewController
@@ -17,6 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"线程安全";
+    
+    //线程不安全 slice并没有预期的1000
+    dispatch_queue_t queue = dispatch_queue_create("TestQueue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        for (int i=0; i<10000; i++) {
+            self.slice = self.slice + 1;
+        }
+        LGNSLog(@"任务1 %ld", self.slice);
+    });
+    dispatch_async(queue, ^{
+        for (int i=0; i<10000; i++) {
+            self.slice = self.slice + 1;
+        }
+        LGNSLog(@"任务2 %ld", self.slice);
+    });
+    
+    LGNSLog(@"任务3 %ld", self.slice);
 }
 
 @end
