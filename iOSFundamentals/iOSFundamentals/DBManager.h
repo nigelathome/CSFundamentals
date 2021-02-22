7cfd86e51b35459996f46cbcc2fef389af7ecb12
Hui,Li <nigelathome@126.com>
2021-02-22 08:04:31 +0800

    导入FMDB

---
+++ iOSFundamentals/iOSFundamentals/DBManager.h
@@ -0,0 +1,20 @@
+//
+//  DBManager.h
+//  iOSFundamentals
+//
+//  Created by Hui,Li on 2021/2/22.
+//  Copyright © 2021 Hui,Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "singleton.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DBManager : NSObject
+
+singleton_h(DBManager)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSFundamentals/iOSFundamentals/DBManager.m
@@ -0,0 +1,15 @@
+//
+//  DBManager.m
+//  iOSFundamentals
+//
+//  Created by Hui,Li on 2021/2/22.
+//  Copyright © 2021 Hui,Li. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+singleton_m(DBManager)
+
+@end
