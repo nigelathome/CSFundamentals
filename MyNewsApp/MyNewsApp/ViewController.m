44e5863c9083a4d6285908a1afc96a91290f043f
nigelli <nigelli@tencent.com>
2021-02-24 08:55:17 +0800

    去掉Main.storyboard和scene。显示添加window并将ViewController作为根控制器

--- MyNewsApp/MyNewsApp/ViewController.m
+++ MyNewsApp/MyNewsApp/ViewController.m
@@ -15,7 +15,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
+    [self.view addSubview:({
+        UILabel *label = [[UILabel alloc] init];
+        label.text = @"hello world!";
+        [label sizeToFit];
+        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+        label;
+    })];
 }
 
 
