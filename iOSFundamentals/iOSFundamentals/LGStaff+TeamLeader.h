b06e0bbf6d00fb782414a2f1f98d9489a7349d4b
Hui, Li <nigelathome@126.com>
2021-03-07 11:56:37 +0800

    分类的使用：扩展原类的能力又不改变原类的结构

---
+++ iOSFundamentals/iOSFundamentals/LGStaff+TeamLeader.h
@@ -0,0 +1,21 @@
+//
+//  LGStaff+TeamLeader.h
+//  iOSFundamentals
+//
+//  Created by Hui,Li on 2021/3/7.
+//  Copyright © 2021 Hui,Li. All rights reserved.
+//
+
+#import "LGStaff.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGStaff (TeamLeader)
+
+- (void)doTeamReport;
+- (void)checkTeamMemberWeeklyReport;
+- (void)developiOSAPP;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSFundamentals/iOSFundamentals/LGStaff+TeamLeader.m
@@ -0,0 +1,28 @@
+//
+//  LGStaff+TeamLeader.m
+//  iOSFundamentals
+//
+//  Created by Hui,Li on 2021/3/7.
+//  Copyright © 2021 Hui,Li. All rights reserved.
+//
+
+#import "LGStaff+TeamLeader.h"
+
+@implementation LGStaff (TeamLeader)
+
+- (void)doTeamReport {
+    LGNSLog(@"分类 %@ %s", [self class], __FUNCTION__);
+}
+
+- (void)checkTeamMemberWeeklyReport {
+    LGNSLog(@"分类 %@ %s", [self class], __FUNCTION__);
+}
+
+/*
+ 分类和原类方法完全相同，运行期该方法会覆盖原类的方法
+ */
+- (void)developiOSAPP {
+    LGNSLog(@"分类 %@ %s", [self class], __FUNCTION__);
+}
+
+@end
